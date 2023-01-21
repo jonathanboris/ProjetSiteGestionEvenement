d6a16ba589c71e93f27f2732e1bbb3b925bb4a56
JBoris <jonathan-boris.ouedraogo@ensg.eu>
2023-01-21 12:59:26 +0100

    started project

---
+++ src/main/java/eu/tsi/jee/GestionEvenement/GestionEvenementApplication.java
@@ -0,0 +1,13 @@
+package eu.tsi.jee.GestionEvenement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEvenementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionEvenementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/tsi/jee/GestionEvenement/GestionEvenementApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.tsi.jee.GestionEvenement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEvenementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
